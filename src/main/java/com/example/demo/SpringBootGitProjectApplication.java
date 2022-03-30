88abfabbab198ffc262a88066358370cc7c107ea
javabydevdutt <javabydevdutt@gmail.com>
2022-03-30 08:08:03 +0530

    changed done on HIS-134

--- src/main/java/com/example/demo/SpringBootGitProjectApplication.java
+++ src/main/java/com/example/demo/SpringBootGitProjectApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootGitProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGitProjectApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringBootGitProjectApplication.class, args);
+		
+		// changes done in HIS-134
+		run.close();
 	}
 
 }
